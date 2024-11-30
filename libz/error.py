04ef6b3ef33ec9eb087ed43f9149e1a1c7935638
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-29 22:47:34 -0600

    #fix: added print for fatal error with code

--- libz/error.py
+++ libz/error.py
@@ -2,5 +2,18 @@ class Error:
     def __init__(self, message):
         self.message = message
 
+    def _print_error(self):
+        print(f"\n{self.message}")
+
     def handle(self):
-        print(f"\n{self.message}\n")
+        self._print_error
+
+
+class FatalError(Error):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def handle(self):
+        self._print_error
+        print("\nExiting the program with exit code 1.\n")
+        exit(1)
