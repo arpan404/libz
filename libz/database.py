842e5a5c4026d29ffc1d3d0f5b2b2a0992697d9c
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-29 22:37:26 -0600

    #add: Added a database class, and also example for defining a schema

--- libz/database.py
+++ libz/database.py
@@ -1,2 +1,38 @@
+from error import Error
+
+
 class Database:
-    pass
+    database_name: str = None
+    database_schema: dict = None
+
+    def __init__(self, database_name):
+        if (Database.database_name is not None):
+            raise Error(
+                "Database is already initialized. Reinitialization is not allowed.")
+        Database.database_name = database_name
+
+    def defineSchema(self, schema):
+        """
+        example:
+        Database.defineSchema(
+            {
+                name: "users",
+                fields:{
+                    "uid": {
+                        type: "string",
+                        unique: True,
+                        primary: True
+                    },
+                    "firstName":{
+                        type:"string",
+                        unique: False
+                    },
+                    "joinedDate":{
+                        type:"date"
+                    }
+                }
+            }
+        )
+        """
+        
+        
