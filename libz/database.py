4ed905fad9866bcabd610c3ec72a93b7efae5390
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-30 14:55:24 -0600

    #refactor: separated logic from main class to database class

--- libz/database.py
+++ libz/database.py
@@ -1,6 +1,57 @@
 from error import Error, FatalError
-from typing import Set
+from typing import Set, List
 
 
 class Database:
-    pass
+    def __init__(self, database_name):
+        self._database_name = database_name
+        self.__schemas: Set[dict] = []
+
+    def _define_schema(self, schemas: List[dict]) -> 'Database':
+        if not schemas:
+            raise FatalError(
+                "Schema is required while calling 'define_schema' method, but got none")
+
+        if isinstance(schemas, dict):
+            self.__create_schema(schemas)
+
+        if isinstance(schemas, list):
+            for schema in schemas:
+                if isinstance(schema, dict):
+                    self.__create_schema(schema)
+                else:
+                    raise FatalError(
+                        "Invalid schema provided. Check documentation for more details.")
+        else:
+            raise FatalError(
+                "Invalid schema provided. Check documentation for more details.")
+
+        return self
+
+    def __create_schema(self, schema: dict) -> None:
+        validated_schema = self.__validate_schema(schema)
+        self.__schemas.add(validated_schema)
+
+    def __validate_schema(self, schema: dict) -> dict:
+        if not schema.name:
+            raise FatalError(
+                "For defining a schema, name is required, which was not provided. Check documentation for more details.")
+        if not isinstance(schema.name, str):
+            raise FatalError(
+                "Schema's name must only be string. Check documentation for more information.")
+
+        if not schema.name.isalpha():
+            raise FatalError(
+                "Schema's name must only be of alphabets. Check documentation for more information.")
+
+        if not schema.fields:
+            raise FatalError(
+                "For defining a schema, fields must be provided. Check documentation for more information.")
+
+        if not isinstance(schema.fields, list):
+            raise FatalError(
+                "For defining a schema, list of fields must be provided. Check documentation for more information.")
+
+        validated_schema: dict = {}
+
+        validated_schema["name"] = schema.name
--- libz/libzmain.py
+++ libz/libzmain.py
@@ -1,59 +1,16 @@
 from error import Error, FatalError
 from typing import List, Set
+from database import Database
 
 
-class Libz:
-    libz_database_name: str = None
+class Libz(Database):
+    libz_databases: Set[str] = {}
 
     def __init__(self, database_name):
-        if Libz.libz_database_name is not None and Libz.libz_database_name == database_name:
+        if Libz.libz_database_name and database_name in Libz.libz_databases:
             raise FatalError(
                 "Same database is reinitialized, which is not allowed."
             )
-        Libz.libz_database_name = database_name
-        self.database_name = database_name
+        super().__init__(database_name)
+        Libz.libz_databases.add(database_name)
         self.database_schema: List[dict] = []
-
-    def define_schema(self, schema: List[dict]) -> 'Libz':
-        if not schema:
-            raise FatalError(
-                "Schema is required while calling 'define_schema' method, but got none")
-
-        if isinstance(schema, dict):
-            validated_schema = self.__validate_schema(schema)
-            self.database_schema.append(validated_schema)
-
-        if isinstance(schema, list):
-            for individual_schema in schema:
-                if isinstance(schema, dict):
-                    validated_schema = self.__validate_schema(
-                        individual_schema)
-                    self.database_schema.append(validated_schema)
-                else:
-                    raise FatalError(
-                        "Invalid schema provided. Check documentation for more details.")
-        else:
-            raise FatalError(
-                "Invalid schema provided. Check documentation for more details.")
-
-    def __validate_schema(schema: dict) -> dict:
-        if not schema.name:
-            raise FatalError(
-                "For defining a schema, name is required, which was not provided. Check documentation for more details.")
-        if not isinstance(schema.name, str):
-            raise FatalError("Schema's name must only be string. Check documentation for more information.")
-
-        if not schema.name.isalpha():
-            raise FatalError("Schema's name must only be of alphabets. Check documentation for more information.")
-        
-        if not schema.fields:
-            raise FatalError("For defining a schema, fields must be provided. Check documentation for more information.")
-
-        if not isinstance(schema.fields, list):
-            raise FatalError("For defining a schema, list of fields must be provided. Check documentation for more information.")
-        
-        validated_schema:dict={}
-        
-        validated_schema["name"] = schema.name
-        
-            
